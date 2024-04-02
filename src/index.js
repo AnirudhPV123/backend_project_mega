b796491109012e4796600e6e68f5797ff10a60d0
AnirudhPV123 <pvanirudh96@gmail.com>
2024-04-02 16:50:46 +0530

    first controller and routes writtern

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
 connectDB()
-  .then(
+  .then(()=>{
     //  app.on is used to check any error occurs globally or in express.js framwork
     app.on("Error", (error) => {
       console.log("ERRR: ", error);
@@ -15,7 +15,7 @@ connectDB()
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     })
-  )
+  })
   .catch((error) => {
     console.log("MONGODB connection FAILED: ", error);
   });
